b678eed662e2e8319e5f0bf68a3ead5d1d45dc36
unknown <teoturner@gmail.com>
2019-03-16 13:30:07 +0000

    used htmlspecialchars function to escape XSS script input

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php 
 
     if(isset($_POST['submit'])){
-        echo $_POST['email']." ";
-        echo $_POST['title']." ";
-        echo $_POST['ingredients']." ";
+        echo htmlspecialchars($_POST['email']." ");
+        echo htmlspecialchars($_POST['title']." ");
+        echo htmlspecialchars($_POST['ingredients']." ");
     }
 
 ?>
