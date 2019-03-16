9a9313d4b32a1cf0bb579147bb2b4b82295b1778
unknown <teoturner@gmail.com>
2019-03-16 13:28:53 +0000

    Created add.php page with a form to learn GET & POST requests echo results back to the page

---
+++ add.php
@@ -0,0 +1,32 @@
+<?php 
+
+    if(isset($_POST['submit'])){
+        echo $_POST['email']." ";
+        echo $_POST['title']." ";
+        echo $_POST['ingredients']." ";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include('templates/header.php') ?>
+
+    <section>
+    <h4>Form</h4>
+    <form action="add.php" method="POST">
+        <label for="email">Email Address:</label>
+        <input type="text" name="email">
+        <label for="title">Pizza Title:</label>
+        <input type="text" name="title">
+        <label for="ingredients">Ingredients (Comma seperated):</label>
+        <input type="text" name="ingredients">
+        <input type="submit" name="submit" value="submit">
+    </form>
+    </section>
+
+    <?php include('templates/footer.php') ?>
+
+    </body>
+</html>
