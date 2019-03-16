db29964f8c01144ef22877a1352d125bee714331
unknown <teoturner@gmail.com>
2019-03-16 13:55:32 +0000

    removed redundant code in header.php and made changes to title

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,4 @@
 
-<?php $name = 'Teo'; $age = 27; ?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +6,4 @@
     <title>Using Header and Footer Templates</title>
 </head>
 <body>
-    <h1>Testing Header</h1>
-
-    <?php echo "working"; ?>
-    <?php echo $name." ".$age; ?>
+    <h1>Pizzas</h1>
