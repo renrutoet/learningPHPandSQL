373807a8fd5c700f3fd795e2dfed3033b9541670
unknown <teoturner@gmail.com>
2019-03-16 10:59:27 +0000

    creating templates

---
+++ templates/footer.php
@@ -0,0 +1,1 @@
+<h4>Testing Footer</h4>
---
+++ templates/header.php
@@ -0,0 +1,14 @@
+
+<?php $name = 'Teo'; $age = 27; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Using Header and Footer Templates</title>
+</head>
+<body>
+    <h1>Testing Header</h1>
+
+    <?php echo "working"; ?>
+    <?php echo $name." ".$age; ?>
